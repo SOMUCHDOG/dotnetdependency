bfec4f584e0a443d1347c21e04037d05ddfcc450
Casey Kawamura <ckawamura@bds.state.id.us>
2024-05-30 09:51:53 -0600

    fixed an issue with new lines on windows and added a function for getting db strings

--- dependencies.sh
+++ dependencies.sh
@@ -13,40 +13,36 @@ cd "$root_directory" || { echo "Directory not found. Exiting."; exit 1; }
 > vbproj_dependencies.txt
 > packages_dependencies.txt
 
-## VB projects
-
-#!/bin/bash
-
-# Create or clear the output files
-> csproj_dependencies.txt
-> vbproj_dependencies.txt
-> packages_dependencies.txt
-> frameworks.txt
-
 # Function to search for dependencies in .csproj files
 find_csproj_dependencies() {
-  grep -r -i -l --include \*.csproj "<PackageReference Include=" ./ >> csproj_dependencies.txt
-  grep -r -i -l --include \*.csproj "<Reference Include=" ./ >> csproj_dependencies.txt
+  grep -r -i -n --include \*.csproj "<PackageReference Include=" ./ | sort | uniq | sed 's/$/\r/' > csproj_dependencies.txt
+  grep -r -i -n --include \*.csproj "<Reference Include=" ./ | sort | uniq | sed 's/$/\r/' > csproj_dependencies.txt
 }
 
 # Function to search for dependencies in .vbproj files
 find_vbproj_dependencies() {
-  grep -r -i -l --include \*.vbproj "<PackageReference Include=" ./ >> vbproj_dependencies.txt
-  grep -r -i -l --include \*.vbproj "<Reference Include=" ./ >> vbproj_dependencies.txt
+  grep -r -i -n --include \*.vbproj "<PackageReference Include=" ./ | sort | uniq | sed 's/$/\r/' > vbproj_dependencies.txt
+  grep -r -i -n --include \*.vbproj "<Reference Include=" ./ | sort | uniq | sed 's/$/\r/' > vbproj_dependencies.txt
 }
 
 # Function to search for dependencies in packages.config files
 find_packages_dependencies() {
-  grep -r -i -l --include \*packages.config "<package id=" ./ >> packages_dependencies.txt
+  grep -r -i -n --include \*packages.config "<package id=" ./ | sort | uniq | sed 's/$/\r/' > packages_dependencies.txt
 }
 
 # Function to search for target frameworks in .csproj and .vbproj files
 find_frameworks() {
-  grep -r -i -l --include \*.csproj "<TargetFramework>" ./ >> frameworks.txt
-  grep -r -i -l --include \*.vbproj "<TargetFramework>" ./ >> frameworks.txt
+  grep -r -i -n --include \*.csproj "<TargetFramework>" ./ | sort | uniq | sed 's/$/\r/' > frameworks.txt
+  grep -r -i -n --include \*.vbproj "<TargetFramework>" ./ | sort | uniq | sed 's/$/\r/' > frameworks.txt
+}
+
+# Function to search for database connection strings
+find_connections() {
+  grep -r -i -n "data source=" ./ | sort | uniq | sed 's/$/\r/' > connectionstrings.txt
 }
 
 # Run the functions
+find_connections
 find_csproj_dependencies
 find_vbproj_dependencies
 find_packages_dependencies
