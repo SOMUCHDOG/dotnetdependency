6313b336c0fad444cdebb5463db20ee8b1b47af3
Casey Kawamura <ckawamura@bds.state.id.us>
2024-05-30 10:47:18 -0600

    Fixed an issue where each grep command was removing the logs from the previous ones

--- dependencies.sh
+++ dependencies.sh
@@ -7,38 +7,39 @@ read -p "Please enter the path to the root directory containing all repositories
 cd "$root_directory" || { echo "Directory not found. Exiting."; exit 1; }
 
 # Create the output files or make sure they are empty
-> connectionstrings.txt
-> csproj_dependencies.txt
-> packages_dependencies.txt
-> vbproj_dependencies.txt
-> packages_dependencies.txt
+>> connectionstrings.txt
+>> csproj_dependencies.txt
+>> packages_dependencies.txt
+>> vbproj_dependencies.txt
+>> packages_dependencies.txt
 
 # Function to search for dependencies in .csproj files
 find_csproj_dependencies() {
-  grep -r -i -n --include \*.csproj "<PackageReference Include=" ./ | sort | uniq | sed 's/$/\r/' > csproj_dependencies.txt
-  grep -r -i -n --include \*.csproj "<Reference Include=" ./ | sort | uniq | sed 's/$/\r/' > csproj_dependencies.txt
+  grep -r -i -n --include \*.csproj "<PackageReference Include=" ./ | sort | uniq | sed 's/$/\r/' >> csproj_dependencies.txt
+  grep -r -i -n --include \*.csproj "<Reference Include=" ./ | sort | uniq | sed 's/$/\r/' >> csproj_dependencies.txt
 }
 
 # Function to search for dependencies in .vbproj files
 find_vbproj_dependencies() {
-  grep -r -i -n --include \*.vbproj "<PackageReference Include=" ./ | sort | uniq | sed 's/$/\r/' > vbproj_dependencies.txt
-  grep -r -i -n --include \*.vbproj "<Reference Include=" ./ | sort | uniq | sed 's/$/\r/' > vbproj_dependencies.txt
+  grep -r -i -n --include \*.vbproj "<PackageReference Include=" ./ | sort | uniq | sed 's/$/\r/' >> vbproj_dependencies.txt
+  grep -r -i -n --include \*.vbproj "<Reference Include=" ./ | sort | uniq | sed 's/$/\r/' >> vbproj_dependencies.txt
 }
 
 # Function to search for dependencies in packages.config files
 find_packages_dependencies() {
-  grep -r -i -n --include \*packages.config "<package id=" ./ | sort | uniq | sed 's/$/\r/' > packages_dependencies.txt
+  grep -r -i -n --include \*packages.config "<package id=" ./ | sort | uniq | sed 's/$/\r/' >> packages_dependencies.txt
 }
 
 # Function to search for target frameworks in .csproj and .vbproj files
 find_frameworks() {
-  grep -r -i -n --include \*.csproj "<TargetFramework>" ./ | sort | uniq | sed 's/$/\r/' > frameworks.txt
-  grep -r -i -n --include \*.vbproj "<TargetFramework>" ./ | sort | uniq | sed 's/$/\r/' > frameworks.txt
+  grep -r -i -n --include \*.csproj "<TargetFramework>>" ./ | sort | uniq | sed 's/$/\r/' >> frameworks.txt
+  grep -r -i -n --include \*.vbproj "<TargetFramework>>" ./ | sort | uniq | sed 's/$/\r/' >> frameworks.txt
 }
 
 # Function to search for database connection strings
 find_connections() {
-  grep -r -i -n "data source=" ./ | sort | uniq | sed 's/$/\r/' > connectionstrings.txt
+  grep -r -i -n "data source=" ./ | sort | uniq | sed 's/$/\r/' >> connectionstrings.txt
+  grep -r -i -n "sql2019" ./ | sort | uniq | sed 's/$/\r/' >> connectionstrings.txt
 }
 
 # Run the functions
@@ -49,7 +50,7 @@ find_packages_dependencies
 find_frameworks
 
 # Combine results into a single file (optional)
-cat csproj_dependencies.txt vbproj_dependencies.txt packages_dependencies.txt frameworks.txt > all_dependencies_and_frameworks.txt
+cat csproj_dependencies.txt vbproj_dependencies.txt packages_dependencies.txt frameworks.txt >> all_dependencies_and_frameworks.txt
 
 # Notify the user
 echo "Dependencies and frameworks have been extracted and saved to csproj_dependencies.txt, vbproj_dependencies.txt, packages_dependencies.txt, frameworks.txt, and all_dependencies_and_frameworks.txt"
